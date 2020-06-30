b30e60bf275ae68485d39fbe866f74cbf1447358
PompousPro <pompouspro@PompousPros-MacBook-Pro.local>
2020-06-29 17:23:15 -0700

    add drawItem method

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { Entypo } from "@expo/vector-icons";
+import { Button } from "native-base";
 import { useFonts } from "@expo-google-fonts/inter";
 
+let itemArray = new Array(9).fill("empty");
+
 export default function App() {
-  const [isCross, setIsCross] = useState(false);
+  const [isX, setIsX] = useState(false);
   const [winMessage, setWinMessage] = useState("");
+
+  drawItem = (itemNumber) => {
+    if (itemArray[itemNumber] === 'empty') {
+      itemArray[itemNumber] = isX;
+      setIsX(!itemArray[itemNumber])
+    }
+  };
+
+  // chooseItemIcon = () => {};
+
+  // chooseItemColor = () => {};
+
+  // resetGame = () => {};
+
+  // winGame = () => {};
 
   return (
     <View style={styles.container}>
